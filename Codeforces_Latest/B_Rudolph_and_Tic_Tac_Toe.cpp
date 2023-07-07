c7be95b2e4e6ecaf3722ab08e1c9d65c6bb332d0
Masud Abdullah <abdullahtamim885@gmail.com>
2023-07-08 00:39:14 +0600

    1846B Div-3

--- Codeforces_Latest/B_Rudolph_and_Tic_Tac_Toe.cpp
+++ Codeforces_Latest/B_Rudolph_and_Tic_Tac_Toe.cpp
@@ -1,95 +1,114 @@
 #include <bits/stdc++.h>
-#define   nl         "\n"
-#define   nn         (cout << "NO\n")
-#define   yy         (cout << "YES\n")
-#define   ss         " "
-#define   ll         long long
-#define   ull        unsigned long long
-#define   gcd(a, b)  __gcd(a, b)
-#define   lcm(a, b)  ((a/gcd(a,b)) * b)
-#define   pii        pair<int,int>
-#define   pll        pair<long long,long long>
-#define   sz(v)      (int)v.size()
-#define   mm(a, x)   memset(a, x, sizeof(a))
-#define   FIO        ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define nl "\n"
+#define nn (cout << "NO\n")
+#define yy (cout << "YES\n")
+#define ss " "
+#define ll long long
+#define ull unsigned long long
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a / gcd(a, b)) * b)
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define sz(v) (int)v.size()
+#define mm(a, x) memset(a, x, sizeof(a))
+#define FIO                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
 /// sort(a,a+n,greater<int>()); Descending sort
-///sort(a,a+n); array sorting[0 base index]
-///reverse(a,a+n); array reverse[0 base index]
-///sort(s.begin(),s.end()); string sorting
-///reverse(s.begin(),s.end()); string reverse
-///int max_element = *max_element(a,a+n);[0 base index]
-///int min_element = *min_element(a,a+n);[0 base index]
-///int max_indx = max_element(a,a+n)-a;[0 base index]
-///int min_indx = min_element(a,a+n)-a;[0 base index]
+/// sort(a,a+n); array sorting[0 base index]
+/// reverse(a,a+n); array reverse[0 base index]
+/// sort(s.begin(),s.end()); string sorting
+/// reverse(s.begin(),s.end()); string reverse
+/// int max_element = *max_element(a,a+n);[0 base index]
+/// int min_element = *min_element(a,a+n);[0 base index]
+/// int max_indx = max_element(a,a+n)-a;[0 base index]
+/// int min_indx = min_element(a,a+n)-a;[0 base index]
 /// isalpha(charracter) ei function int value return (0,1,2) kore.jodi >0 value return kore taile charracter ta alphabet ar naile alphabet na.
-///isalpha() 1 return korle charracter ta capital ar 2 return korle charracter ta small.
-///Substring_Check:
-///str1(main string)
-///str2(substring)
-///if((str1.find(str2) != string :: npos))yy;
-///else nn;
+/// isalpha() 1 return korle charracter ta capital ar 2 return korle charracter ta small.
+/// Substring_Check:
+/// str1(main string)
+/// str2(substring)
+/// if((str1.find(str2) != string :: npos))yy;
+/// else nn;
 
-///int a[n];
-///iota(a, a + n, 1);  >>>>>>>>>>>> eta diye n length er ekta array te 1-n porjonto value gulo sequentially set kora jay.
+/// int a[n];
+/// iota(a, a + n, 1);  >>>>>>>>>>>> eta diye n length er ekta array te 1-n porjonto value gulo sequentially set kora jay.
 using namespace std;
 int main()
 {
     FIO;
     int t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
         string a[3];
-        for(int i=0;i<3;i++)
+        for (int i = 0; i < 3; i++)
         {
-            for(int j=0;j<3;j++)
+            for (int j = 0; j < 3; j++)
             {
-                cin>>a[i][j];
+                cin >> a[i][j];
             }
         }
 
-        if(a[0][0]==a[0][1] && a[0][0]==a[0][2])
+        if (a[0][0] == a[0][1] && a[0][0] == a[0][2])
         {
-            if(a[0][0]=='.')cout<<"DRAW\n";
-            else cout<<a[0][0]<<nl;
+            if (a[0][0] == '.')
+                cout << "DRAW\n";
+            else
+                cout << a[0][0] << nl;
         }
-        else if(a[1][0]==a[1][1] && a[1][0]==a[1][2])
+        else if (a[1][0] == a[1][1] && a[1][0] == a[1][2])
         {
-            if(a[1][0]=='.')cout<<"DRAW\n";
-            else cout<<a[1][0]<<nl;
+            if (a[1][0] == '.')
+                cout << "DRAW\n";
+            else
+                cout << a[1][0] << nl;
         }
-        else if(a[2][0]==a[2][1] && a[2][0]==a[2][2])
+        else if (a[2][0] == a[2][1] && a[2][0] == a[2][2])
         {
-            if(a[2][0]=='.')cout<<"DRAW\n";
-            else cout<<a[2][0]<<nl;
+            if (a[2][0] == '.')
+                cout << "DRAW\n";
+            else
+                cout << a[2][0] << nl;
         }
-        else if(a[0][0]==a[1][0] && a[0][0]==a[2][0])
+        else if (a[0][0] == a[1][0] && a[0][0] == a[2][0])
         {
-            if(a[0][0]=='.')cout<<"DRAW\n";
-            else cout<<a[0][0]<<nl;
+            if (a[0][0] == '.')
+                cout << "DRAW\n";
+            else
+                cout << a[0][0] << nl;
         }
-        else if(a[0][1]==a[1][1] && a[0][1]==a[2][1])
+        else if (a[0][1] == a[1][1] && a[0][1] == a[2][1])
         {
-            if(a[0][1]=='.')cout<<"DRAW\n";
-            else cout<<a[0][1]<<nl;
+            if (a[0][1] == '.')
+                cout << "DRAW\n";
+            else
+                cout << a[0][1] << nl;
         }
-        else if(a[0][2]==a[1][2] && a[0][2]==a[2][2])
+        else if (a[0][2] == a[1][2] && a[0][2] == a[2][2])
         {
-            if(a[0][2]=='.')cout<<"DRAW\n";
-            else cout<<a[0][2]<<nl;
+            if (a[0][2] == '.')
+                cout << "DRAW\n";
+            else
+                cout << a[0][2] << nl;
         }
-        else if(a[0][0]==a[1][1] && a[0][0]==a[2][2])
+        else if (a[0][0] == a[1][1] && a[0][0] == a[2][2])
         {
-            if(a[0][0]=='.')cout<<"DRAW\n";
-            else cout<<a[0][0]<<nl;
+            if (a[0][0] == '.')
+                cout << "DRAW\n";
+            else
+                cout << a[0][0] << nl;
         }
-        else if(a[0][2]==a[1][1] && a[0][2]==a[2][0])
+        else if (a[0][2] == a[1][1] && a[0][2] == a[2][0])
         {
-            if(a[0][2]=='.')cout<<"DRAW\n";
-            else cout<<a[0][2]<<nl;
+            if (a[0][2] == '.')
+                cout << "DRAW\n";
+            else
+                cout << a[0][2] << nl;
         }
-        else cout<<"DRAW\n";
+        else
+            cout << "DRAW\n";
     }
     return 0;
 }
-
