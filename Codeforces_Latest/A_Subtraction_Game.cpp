d1b4686475c3c9ef73d2d19608fb2fda709491f2
Masud Abdullah <abdullahtamim885@gmail.com>
2023-07-12 08:01:10 +0600

    1844A Div-2

---
+++ Codeforces_Latest/A_Subtraction_Game.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << nl;
+    }
+    return 0;
+}
+
+// #include <bits/stdc++.h>
+// #define nl "\n"
+// #define FIO                       \
+//     ios_base::sync_with_stdio(0); \
+//     cin.tie(0);                   \
+//     cout.tie(0);
+// using namespace std;
+// int main()
+// {
+//     FIO;
+//     int t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         int a, b;
+//         cin >> a >> b;
+//         if (a == 1)
+//             cout << b + 1 << nl;
+//         else
+//             cout << a - 1 << nl;
+//     }
+//     return 0;
+// }
