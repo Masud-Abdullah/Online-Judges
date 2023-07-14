fedbd3d37bbd8517b0926cedab4ef2c704e19818
Masud Abdullah <abdullahtamim885@gmail.com>
2023-07-14 16:50:45 +0600

    1829D Div-4

---
+++ Codeforces_Latest/D_Gold_Rush.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define nl "\n"
+#define nn (cout << "NO\n")
+#define yy (cout << "YES\n")
+#define ss " "
+#define ll long long
+#define ull unsigned long long
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a / gcd(a, b)) * b)
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define sz(v) (int)v.size()
+#define mm(a, x) memset(a, x, sizeof(a))
+#define FIO                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+using namespace std;
+bool gold(ll n, ll m)
+{
+    if (n == m)
+        return true;
+
+    if (n < m || n % 3 != 0)
+        return false;
+
+    bool left = gold((n / 3), m);
+    bool right = gold((n / 3) * 2, m);
+    if (left == true || right == true)
+        return true;
+    else
+        return false;
+}
+int main()
+{
+    FIO;
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n, m;
+        cin >> n >> m;
+
+        if (gold(n, m))
+            yy;
+        else
+            nn;
+    }
+    return 0;
+}
