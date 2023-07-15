e403c9e25c7fd36b733d38378ed3b121aef0c8b6
Masud Abdullah <abdullahtamim885@gmail.com>
2023-07-16 00:58:05 +0600

    368B Div-2

--- Codeforces_Latest/D_Gold_Rush.cpp
+++ Codeforces_Latest/D_Gold_Rush.cpp
@@ -1,3 +1,53 @@
+// #include <bits/stdc++.h>
+// #define nl "\n"
+// #define nn (cout << "NO\n")
+// #define yy (cout << "YES\n")
+// #define ss " "
+// #define ll long long
+// #define ull unsigned long long
+// #define gcd(a, b) __gcd(a, b)
+// #define lcm(a, b) ((a / gcd(a, b)) * b)
+// #define pii pair<int, int>
+// #define pll pair<long long, long long>
+// #define sz(v) (int)v.size()
+// #define mm(a, x) memset(a, x, sizeof(a))
+// #define FIO                       \
+//     ios_base::sync_with_stdio(0); \
+//     cin.tie(0);                   \
+//     cout.tie(0);
+// using namespace std;
+// bool gold(ll n, ll m)
+// {
+//     if (n == m)
+//         return true;
+
+//     if (n < m || n % 3 != 0)
+//         return false;
+
+//     bool left = gold((n / 3), m);
+//     bool right = gold((n / 3) * 2, m);
+//     if (left == true || right == true)
+//         return true;
+//     else
+//         return false;
+// }
+// int main()
+// {
+//     FIO;
+//     ll t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         ll n, m;
+//         cin >> n >> m;
+
+//         if (gold(n, m) == true)
+//             yy;
+//         else
+//             nn;
+//     }
+//     return 0;
+// }
 #include <bits/stdc++.h>
 #define nl "\n"
 #define nn (cout << "NO\n")
@@ -22,14 +72,11 @@ bool gold(ll n, ll m)
         return true;
 
     if (n < m || n % 3 != 0)
-        return false;
+        return false;    
 
     bool left = gold((n / 3), m);
     bool right = gold((n / 3) * 2, m);
-    if (left == true || right == true)
-        return true;
-    else
-        return false;
+    return (left || right);
 }
 int main()
 {
---
+++ Codeforces_Latest/G_Gravity.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define   FIO        ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    FIO;
+    long long n,m;
+    cin>>n>>m;
+    string s[n+1];
+    for(int i=0;i<n;i++)
+    {
+            cin>>s[i];
+    }
+
+    // for(int i=0;i<n;i++)
+    // {
+    //     cout<<s[i]<<endl;
+    // }
+
+   
+    return 0;
+}
