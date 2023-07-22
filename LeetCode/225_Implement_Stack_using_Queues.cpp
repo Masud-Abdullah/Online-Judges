4352ed9787403f99b1453e34a6e78a6f5b768537
Masud Abdullah <abdullahtamim885@gmail.com>
2023-07-22 12:05:00 +0600

    Implement Stack using Queues

--- LeetCode/225_Implement_Stack_using_Queues.cpp
+++ LeetCode/225_Implement_Stack_using_Queues.cpp
@@ -1,54 +1,55 @@
-// class MyStack {
-// public:
-//     queue<int>q;
-//     MyStack() {
+class MyStack {
+public:
+    queue<int>q;
+    MyStack() {
         
-//     }
+    }
     
-//     void push(int x) {
-//         q.push(x);
-//     }
+    void push(int x) {
+        q.push(x);
+    }
     
-//     int pop() {
-//         queue<int>newQ;
-//         int last_element;
-//         while(!q.empty())
-//         {
-//             int val=q.front();
-//             q.pop();
-//             if(q.empty())
-//             {
-//                 last_element = val;
-//                 break;
-//             }
-//             newQ.push(val);
-//         }
-//         q = newQ;
-//         return last_element;
-//     }
+    int pop() {
+        queue<int>newQ;
+        int last_element;
+        while(!q.empty())
+        {
+            int val=q.front();
+            q.pop();
+            if(q.empty())
+            {
+                last_element = val;
+                break;
+            }
+            newQ.push(val);
+        }
+        q = newQ;
+        return last_element;
+    }
     
-//     int top() {
-//         queue<int>newQ;
-//         int last_element;
-//         while(!q.empty())
-//         {
-//             int val=q.front();
-//             q.pop();
-//             if(q.empty())
-//             {
-//                 last_element = val;
-//             }
-//             newQ.push(val);
-//         }
-//         q = newQ;
-//         return last_element;
-//     }
+    int top() {
+        queue<int>newQ;
+        int last_element;
+        while(!q.empty())
+        {
+            int val=q.front();
+            q.pop();
+            if(q.empty())
+            {
+                last_element = val;
+            }
+            newQ.push(val);
+        }
+        q = newQ;
+        return last_element;
+    }
     
-//     bool empty() {
-//         if(q.empty())return true;
-//         else return false;   
-//     }
-// };
+    bool empty() {
+        if(q.empty())return true;
+        else return false;   
+    }
+    
+};
 
 /**
  * Your MyStack object will be instantiated and called as such:
