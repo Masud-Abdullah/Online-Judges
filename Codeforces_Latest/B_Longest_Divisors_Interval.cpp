bdbca6544878447d3f6b05898a2ba9e9dadb945d
Masud Abdullah <abdullahtamim885@gmail.com>
2023-07-29 23:55:09 +0600

    1855B Codeforces Round 889 (Div. 2)

--- Codeforces_Latest/B_Longest_Divisors_Interval.cpp
+++ Codeforces_Latest/B_Longest_Divisors_Interval.cpp
@@ -1,66 +1,70 @@
 #include <bits/stdc++.h>
-#define   nl         "\n"
-#define   nn         (cout << "NO\n")
-#define   yy         (cout << "YES\n")
-#define   ss         " "
-#define   ll         long long
-#define   ull        unsigned long long
-#define   gcd(a, b)  __gcd(a, b)
-#define   lcm(a, b)  ((a/gcd(a,b)) * b)
-#define   pii        pair<int,int>
-#define   pll        pair<long long,long long>
-#define   sz(v)      (int)v.size()
-#define   mm(a, x)   memset(a, x, sizeof(a))
-#define   FIO        ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define nl "\n"
+#define nn (cout << "NO\n")
+#define yy (cout << "YES\n")
+#define ss " "
+#define ll long long
+#define ull unsigned long long
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a / gcd(a, b)) * b)
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define sz(v) (int)v.size()
+#define mm(a, x) memset(a, x, sizeof(a))
+#define FIO                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
 /// sort(a,a+n,greater<int>()); Descending sort
-///sort(a,a+n); array sorting[0 base index]
-///reverse(a,a+n); array reverse[0 base index]
-///sort(s.begin(),s.end()); string sorting
-///reverse(s.begin(),s.end()); string reverse
-///int max_element = *max_element(a,a+n);[0 base index]
-///int min_element = *min_element(a,a+n);[0 base index]
-///int max_indx = max_element(a,a+n)-a;[0 base index]
-///int min_indx = min_element(a,a+n)-a;[0 base index]
+/// sort(a,a+n); array sorting[0 base index]
+/// reverse(a,a+n); array reverse[0 base index]
+/// sort(s.begin(),s.end()); string sorting
+/// reverse(s.begin(),s.end()); string reverse
+/// int max_element = *max_element(a,a+n);[0 base index]
+/// int min_element = *min_element(a,a+n);[0 base index]
+/// int max_indx = max_element(a,a+n)-a;[0 base index]
+/// int min_indx = min_element(a,a+n)-a;[0 base index]
 /// isalpha(charracter) ei function int value return (0,1,2) kore.jodi >0 value return kore taile charracter ta alphabet ar naile alphabet na.
-///isalpha() 1 return korle charracter ta capital ar 2 return korle charracter ta small.
+/// isalpha() 1 return korle charracter ta capital ar 2 return korle charracter ta small.
 /// for converting string to integer: stoi(a); [a is the string ]
-///Substring_Check:
-///str1(main string)
-///str2(substring)
-///if((str1.find(str2) != string :: npos))yy;
-///else nn;
+/// Substring_Check:
+/// str1(main string)
+/// str2(substring)
+/// if((str1.find(str2) != string :: npos))yy;
+/// else nn;
 
-///int a[n];
-///iota(a, a + n, 1);  >>>>>>>>>>>> eta diye n length er ekta array te 1-n porjonto value gulo sequentially set kora jay.
+/// int a[n];
+/// iota(a, a + n, 1);  >>>>>>>>>>>> eta diye n length er ekta array te 1-n porjonto value gulo sequentially set kora jay.
 using namespace std;
 int main()
 {
     FIO;
     ll t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
         ll n;
-        cin>>n;
-        if(n==1)
+        cin >> n;
+        if (n == 1)
         {
-            cout<<1<<nl;
+            cout << 1 << nl;
             continue;
         }
-        ll cnt=0,mx=0;
-        for(ll i=1;i<=1000;i++)
+        ll cnt = 0, mx = 0;
+        for (ll i = 1; i <= 5000; i++)
         {
-            if(n%i==0)cnt++;
+            if (n % i == 0)
+                cnt++;
             else
             {
-                //mx=max(cnt,mx);
-                if(cnt>mx)mx=cnt;
-                cnt=0;
+                // mx=max(cnt,mx);
+                if (cnt > mx)
+                    mx = cnt;
+                cnt = 0;
             }
         }
 
-        cout<<mx<<nl;
+        cout << mx << nl;
     }
     return 0;
 }
-
