28084629e22466a34da585f0d40ec8c203d73d0c
Masud Abdullah <abdullahtamim885@gmail.com>
2023-08-02 22:33:19 +0600

    2021 ICPC Asia Taiwan Online Programming Contest

--- Codeforces_Latest/B_Aliquot_Sum.cpp
+++ Codeforces_Latest/B_Aliquot_Sum.cpp
@@ -15,72 +15,37 @@
     ios_base::sync_with_stdio(0); \
     cin.tie(0);                   \
     cout.tie(0);
-/// sort(a,a+n,greater<int>()); Descending sort
-/// sort(a,a+n); array sorting[0 base index]
-/// reverse(a,a+n); array reverse[0 base index]
-/// sort(s.begin(),s.end()); string sorting
-/// reverse(s.begin(),s.end()); string reverse
-/// int max_element = *max_element(a,a+n);[0 base index]
-/// int min_element = *min_element(a,a+n);[0 base index]
-/// int max_indx = max_element(a,a+n)-a;[0 base index]
-/// int min_indx = min_element(a,a+n)-a;[0 base index]
-/// isalpha(charracter) ei function int value return (0,1,2) kore.jodi >0 value return kore taile charracter ta alphabet ar naile alphabet na.
-/// isalpha() 1 return korle charracter ta capital ar 2 return korle charracter ta small.
-/// for converting string to integer: stoi(a); [a is the string ]
-/// Substring_Check:
-/// str1(main string)
-/// str2(substring)
-/// if((str1.find(str2) != string :: npos))yy;
-/// else nn;
-
-/// int a[n];
-/// iota(a, a + n, 1);  >>>>>>>>>>>> eta diye n length er ekta array te 1-n porjonto value gulo sequentially set kora jay.
 using namespace std;
+const int MX = 1e6 + 9;
+ll cnt[MX];
+void divisorSum()
+{
+    for (int i = 1; i <= MX; i++)
+    {
+        for (int j = i; j <= MX; j += i)
+        {
+            cnt[j] += i;
+        }
+    }
+}
 int main()
 {
     FIO;
     int t;
     cin >> t;
+    divisorSum();
     while (t--)
     {
-        int cnt[100007] = {0};
-        int n;
+        ll n;
         cin >> n;
-        for (int i = 1; i <= sqrt(n); i++)
-        {
-            if (n % i == 0)
-            {
-                cnt[i]++;
-                cnt[n / i]++;
-            }
-        }
-
-        ll sum = 1;
-        for (int i = 2; i <= sqrt(n); i++)
-        {
-            if (cnt[i] > 0)
-            {
-                sum += i;
-                if (i != n / i && cnt[n / i] > 0)
-                    sum += (n / i);
-                cnt[n / i] = 0;
-            }
-        }
-        // cout<<sum<<nl;
-
-        if (sum > n)
+        // cout<<cnt[n]<<nl;
+        ll ans = cnt[n] - n;
+        if (ans > n)
             cout << "abundant\n";
-        else if (sum < n)
+        else if (ans < n)
             cout << "deficient\n";
         else
             cout << "perfect\n";
-
-        // for(int i=1;i<=n;i++)
-        // {
-        //     cout<<cnt[i]<<" ";
-        //     //if(cnt[i]>0)cout<<i<<" ";
-        // }
-        // cout<<nl;
     }
     return 0;
 }
