435aa9aa5bbd5e2d3ffa14704f697c701867acdf
Masud Abdullah <abdullahtamim885@gmail.com>
2023-08-02 14:19:59 +0600

    LightOJ - 1109

--- LightOJ/False_Ordering.cpp
+++ LightOJ/False_Ordering.cpp
@@ -1,3 +1,66 @@
+// #include <bits/stdc++.h>
+// #define nl "\n"
+// #define nn (cout << "NO\n")
+// #define yy (cout << "YES\n")
+// #define ss " "
+// #define ll long long
+// #define ull unsigned long long
+// #define gcd(a, b) __gcd(a, b)
+// #define lcm(a, b) ((a / gcd(a, b)) * b)
+// #define pii pair<int, int>
+// #define pll pair<long long, long long>
+// #define sz(v) (int)v.size()
+// #define mm(a, x) memset(a, x, sizeof(a))
+// #define FIO                       \
+//     ios_base::sync_with_stdio(0); \
+//     cin.tie(0);                   \
+//     cout.tie(0);
+// using namespace std;
+// pll cnt[1005];
+// bool cmp(pii a, pii b)
+// {
+//     if (a.first < b.first)
+//         return true;
+//     else if (a.first == b.first)
+//     {
+//         if (a.second > b.second)
+//             return true;
+//         else
+//             return false;
+//     }
+//     else
+//         return false;
+// }
+// void get_divisor()
+// {
+//     for (int i = 1; i <= 1000; i++)
+//     {
+//         cnt[i].second = i;
+//         for (int j = 1; j * j <= i; j++)
+//         {
+//             if (i % j == 0 && j * j == i)
+//                 cnt[i].first++;
+//             else if (i % j == 0)
+//                 cnt[i].first += 2;
+//         }
+//     }
+//     sort(cnt, cnt + 1001, cmp);
+// }
+// int main()
+// {
+//     FIO;
+//     ll t;
+//     cin >> t;
+//     get_divisor();
+//     for (int i = 1; i <= t; i++)
+//     {
+//         cout << "Case " << i << ": ";      //Complexity: N(sqrt(N))
+//         ll n;
+//         cin >> n;
+//         cout << cnt[n].second << nl;
+//     }
+//     return 0;
+// }
 #include <bits/stdc++.h>
 #define nl "\n"
 #define nn (cout << "NO\n")
@@ -19,29 +82,18 @@ using namespace std;
 pll cnt[1005];
 bool cmp(pii a, pii b)
 {
-    if (a.first < b.first)
-        return true;
-    else if (a.first == b.first)
-    {
-        if (a.second > b.second)
-            return true;
-        else
-            return false;
-    }
-    else
-        return false;
+    if (a.first == b.first)
+        return a.second > b.second;
+    return a.first < b.first;
 }
 void get_divisor()
 {
     for (int i = 1; i <= 1000; i++)
     {
         cnt[i].second = i;
-        for (int j = 1; j * j <= i; j++)
+        for (int j = i; j <= 1000; j += i)
         {
-            if (i % j == 0 && j * j == i)
-                cnt[i].first++;
-            else if (i % j == 0)
-                cnt[i].first += 2;
+            cnt[j].first++;
         }
     }
     sort(cnt, cnt + 1001, cmp);
@@ -54,7 +106,7 @@ int main()
     get_divisor();
     for (int i = 1; i <= t; i++)
     {
-        cout << "Case " << i << ": ";
+        cout << "Case " << i << ": "; // Complexity: NlogN
         ll n;
         cin >> n;
         cout << cnt[n].second << nl;
