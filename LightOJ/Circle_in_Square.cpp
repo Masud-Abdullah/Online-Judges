d11f039fef4961e42c02e0b9e6979b471af9a967
Masud Abdullah <abdullahtamim885@gmail.com>
2023-08-04 08:02:15 +0600

    LOJ-1022

---
+++ LightOJ/Circle_in_Square.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define nl "\n"
+#define nn (cout << "NO\n")
+#define yy (cout << "YES\n")
+#define ss " "
+#define ll long long
+#define ull unsigned long long
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a / gcd(a, b)) * b)
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define sz(v) v.size()
+#define mm(a, x) memset(a, x, sizeof(a))
+#define FIO                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+using namespace std;
+int main()
+{
+    FIO;
+
+    int t;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+        double r;
+        cin >> r;
+        double pi = 2 * acos(0.0);
+        double ans = ((2 * r) * (2 * r)) - (pi * (r)*r);
+        printf("Case %d: %0.2f\n", i, ans);
+    }
+    return 0;
+}
