254d6afaac8027d670022986f1e653610b3d342f
Masud Abdullah <abdullahtamim885@gmail.com>
2023-08-04 16:29:32 +0600

    LOJ-1182

---
+++ LightOJ/Parity.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define nl "\n"
+#define nn (cout << "NO\n")
+#define yy (cout << "YES\n")
+#define ss " "
+#define ll long long
+#define ull unsigned long long
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a / gcd(a, b)) * b)
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define sz(v) (int)v.size()
+#define mm(a, x) memset(a, x, sizeof(a))
+#define FIO                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+using namespace std;
+int main()
+{
+    FIO;
+    ll t;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+        cout << "Case " << i << ": ";
+        int n;
+        cin >> n;
+        if (__builtin_popcount(n) % 2 == 0) // This function is used to count the number of one’s(set bits) in an integer.
+            cout << "even\n";
+        else
+            cout << "odd\n";
+    }
+    return 0;
+}
