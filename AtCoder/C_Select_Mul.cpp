48f579cb9bed5b074b4d6a5acbfcd90e25b13581
Masud Abdullah <abdullahtamim885@gmail.com>
2023-08-22 16:43:06 +0600

    abc221 C

---
+++ AtCoder/C_Select_Mul.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#define nl "\n"
+#define NN (cout << "NO\n")
+#define YY (cout << "YES\n")
+#define nn (cout << "No\n")
+#define yy (cout << "Yes\n")
+#define ss second
+#define ff first
+#define all(v) v.begin(), v.end()
+#define ll long long int
+#define ull unsigned long long
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a / gcd(a, b)) * b)
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vii vector<int>
+#define vll vector<ll>
+#define vpi vector<pii>
+#define vpl vector<pll>
+#define sz(v) v.size()
+#define mm(a, x) memset(a, x, sizeof(a))
+#define pi acos(-1.0)
+#define ceil(a, b) (a + b - 1) / b
+#define jog(v) accumulate(v.begin(), v.end(), 0)
+#define gun(v) accumulate(v.begin(), v.end(), 1, multiplies<int>())
+#define FIO                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+using namespace std;
+int main()
+{
+    FIO;
+    string s;
+    cin >> s;
+    sort(s.begin(), s.end());
+    reverse(all(s));
+    int a = s[0] - 48;
+    int b = s[1] - 48;
+    bool f = true;
+    int cnt = 0;
+    for (int i = 2; i < sz(s); i++)
+    {
+        if (a < b)
+        {
+            a = (a * 10) + s[i] - 48;
+        }
+        else
+            b = (b * 10) + s[i] - 48;
+    }
+
+    cout << a * b << nl;
+
+    return 0;
+}
