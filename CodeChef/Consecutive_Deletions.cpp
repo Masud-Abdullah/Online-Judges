7ba480f70f7ef8e4976e33008fb10ddaa28cb834
Masud Abdullah <abdullahtamim885@gmail.com>
2023-08-23 21:08:55 +0600

    Consecutive Deletions

---
+++ CodeChef/Consecutive_Deletions.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#define nl "\n"
+#define NN (cout << "NO\n")
+#define YY (cout << "YES\n")
+#define nn (cout << "No\n")
+#define yy (cout << "Yes\n")
+#define ss second
+#define ff first
+#define all(v) v.begin(), v.end()
+#define ll long long int
+#define ull unsigned long long
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a / gcd(a, b)) * b)
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vii vector<int>
+#define vll vector<ll>
+#define vpi vector<pii>
+#define vpl vector<pll>
+#define sz(v) v.size()
+#define mm(a, x) memset(a, x, sizeof(a))
+#define pi acos(-1.0)
+#define ceil(a, b) (a + b - 1) / b
+#define jog(v) accumulate(v.begin(), v.end(), 0)
+#define gun(v) accumulate(v.begin(), v.end(), 1, multiplies<int>())
+#define FIO                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+using namespace std;
+int main()
+{
+    FIO;
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n, k, sm = 0;
+        cin >> n >> k;
+        ll a[n + 1];
+        for (int i = 1; i <= n; i++)
+        {
+            cin >> a[i];
+            sm += a[i];
+        }
+
+        a[0] = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            a[i] += a[i - 1];
+        }
+
+        ll mn = LLONG_MAX, L = 0;
+        for (int i = k; i <= n; i++)
+        {
+            mn = min(mn, a[i] - a[L]);
+            L++;
+        }
+
+        sm -= mn;
+
+        ll ans = sm + ((mn * (mn + 1)) / 2);
+        cout << ans << nl;
+    }
+    return 0;
+}
