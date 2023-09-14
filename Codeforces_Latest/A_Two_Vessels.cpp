cdada43a46e6e71915062eef3ac3b6ff2d5d80a6
Masud Abdullah <abdullahtamim885@gmail.com>
2023-09-14 13:35:54 +0600

    1872A Codeforces Round 895 (Div. 3)

---
+++ Codeforces_Latest/A_Two_Vessels.cpp
@@ -0,0 +1,140 @@
+#include <bits/stdc++.h>
+#define nl "\n"
+#define NN (cout << "NO\n")
+#define YY (cout << "YES\n")
+#define nn (cout << "No\n")
+#define yy (cout << "Yes\n")
+#define ss second
+#define ff first
+#define all(v) v.begin(), v.end()
+#define ll long long int
+#define ull unsigned long long
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a / gcd(a, b)) * b)
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vii vector<int>
+#define vll vector<ll>
+#define vpi vector<pii>
+#define vpl vector<pll>
+#define sz(v) v.size()
+#define mm(a, x) memset(a, x, sizeof(a))
+#define pi acos(-1.0)
+#define ceil(a, b) (a + b - 1) / b
+#define jog(v) accumulate(v.begin(), v.end(), 0)
+#define gun(v) accumulate(v.begin(), v.end(), 1, multiplies<int>())
+#define FIO                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+/// sort(a,a+n,greater<int>()); Descending sort
+/// sort(a,a+n); array sorting[0 base index]
+/// reverse(a,a+n); array reverse[0 base index]
+/// sort(s.begin(),s.end()); string sorting
+/// reverse(s.begin(),s.end()); string reverse
+/// int max_element = *max_element(a,a+n);[0 base index]
+/// int min_element = *min_element(a,a+n);[0 base index]
+/// int max_indx = max_element(a,a+n)-a;[0 base index]
+/// int min_indx = min_element(a,a+n)-a;[0 base index]
+/// isalpha(charracter) ei function int value return (0,1,2) kore.jodi >0 value return kore taile charracter ta alphabet ar naile alphabet na.
+/// isalpha() 1 return korle charracter ta capital ar 2 return korle charracter ta small.
+/// Substring_Check:
+/// str1(main string)
+/// str2(substring)
+/// if((str1.find(str2) != string :: npos))yy;
+/// else nn;
+/// for converting string to integer: stoi(a); [a is the string ]
+/// int a[n];
+/// iota(a, a + n, 1);  >>>>>>>>>>>> eta diye n length er ekta array te 1-n porjonto value gulo sequentially set kora jay.
+/// kadane algorithm for maximum subarray sum
+/// __builtin_popcount(x) (__builtin_popcountll(x) for long long data type) ---->>> This function is used to count the number of one’s(set bits) in an integer.
+
+/// __builtin_parity(x) (__builtin_parityll(x) for long long data type)   ---->>> This function is used to check the parity of a number.
+/// This function returns true(1) if the number has odd parity else it returns false(0) for even parity.
+/// parity means the number of one's of it's binary representaion is even or odd.
+
+///__builtin_clz(x)  (__builtin_clzll(x) for long long data type)  ---->>> This function is used to count the leading zeros of the integer.
+
+///__builtin_ctz(x)   (__builtin_ctzll(x) for long long data type)  ---->>> This function is used to count the trailing zeros of the integer.
+
+using namespace std;
+int main()
+{
+    FIO;
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll a, b, c;
+        cin >> a >> b >> c;
+
+        ll df = abs(a - b);
+
+        if (df == 0)
+        {
+            cout << 0 << nl;
+            continue;
+        }
+
+        ll ans = df / c;
+
+        if (df % c)
+            ans++;
+
+        ans++;
+
+        ans /= 2;
+
+        cout << ans << nl;
+    }
+    return 0;
+}
+// Abid vai's code
+/*
+#include<bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+#define endl '\n'
+#define ll long long
+#define pii pair<int,int>
+#define fastIO ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T> using o_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+//freopen("diamond.in","r",stdin);
+//freopen("diamond.out","w",stdout);
+
+
+
+int main()
+{
+    int t, a, b, c, ans;
+
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> a >> b >> c;
+        ans = 0;
+
+        if (a > b)swap(a, b);
+
+        while (a < b)
+        {
+            a += c;
+            b -= c;
+            ans++;
+        }
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
+*/
